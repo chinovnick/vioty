df5fc36bb1c551f2325cb95e6ef2078da5ace611
chinovnick <gagahahaha663@gmail.com>
2026-03-06 21:09:38 +0400

    адаптив

--- script.js
+++ script.js
@@ -19,3 +19,28 @@ prev.addEventListener("click", () => {
 function bob() {
     window.location.href = window.location.href;
 }
+
+
+
+const menuBtn = document.getElementById("menu-button");
+const closeBtn = document.getElementById("close-button");
+const navMenu = document.getElementById("nav-menu");
+
+menuBtn.addEventListener("click", () => {
+    navMenu.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+});
+
+
+function openTab() {
+  document.getElementById("myTab").style.display = "flex";
+  document.getElementById("overlay").style.display = "block";
+}
+
+function closeTab() {
+  document.getElementById("myTab").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
